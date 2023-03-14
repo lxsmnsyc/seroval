cbfc95436789e081b31a6bad6fd235a632deec7a
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-03-14 19:34:48 +0800

    fix README

--- packages/seroval/test.js
+++ packages/seroval/test.js
@@ -2,6 +2,7 @@ import { serialize } from 'seroval';
 
 const y = Object.create(null);
 y.self = y;
+y.example = 'Hello World';
 
 function serializeWithTarget(value, target) {
   console.log('Target is', target)
@@ -13,4 +14,7 @@ function serializeWithTarget(value, target) {
 
 serializeWithTarget(y, 'es5');
 serializeWithTarget(y, 'es2023');
-
+// Target is es5
+// (function(h){return (h=Object.create(null),h.self=h,h.example="Hello World",h)})()
+// Target is es2023
+// (h=>(h=Object.assign(Object.create(null),{example:"Hello World"}),h.self=h,h))()
