11f52551bf16b8e927c7d4f8d9fb3f0adbff6e5f
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-04-09 15:53:43 +0800

    Fix string serialization

--- packages/seroval/src/string.ts
+++ packages/seroval/src/string.ts
@@ -1,30 +1,53 @@
-const SAFE_SERIALIZE_CHECK = /<|\u2028|\u2029/;
-
-export function serializeChar(str: string): string {
+export function serializeChar(str: string) {
   switch (str) {
-    case '<':
-      return '\\x3C';
-    case '\u2028':
-      return '\\u2028';
-    case '\u2029':
-      return '\\u2029';
-    default:
-      return str;
+    case '"': return '\\"';
+    case '\\': return '\\\\';
+    case '\n': return '\\n';
+    case '\r': return '\\r';
+    case '\b': return '\\b';
+    case '\t': return '\\t';
+    case '\f': return '\\f';
+    case '<': return '\\x3C';
+    case '\u2028': return '\\u2028';
+    case '\u2029': return '\\u2029';
+    default: return undefined;
   }
 }
 
+// Written by https://github.com/DylanPiercey and is distributed under the MIT license.
+// Creates a JavaScript double quoted string and escapes all characters
+// not listed as DoubleStringCharacters on
+// Also includes "<" to escape "</script>" and "\" to avoid invalid escapes in the output.
+// http://www.ecma-international.org/ecma-262/5.1/#sec-7.8.4
 export function serializeString(str: string) {
-  const safe = JSON.stringify(str);
-  const unquoted = safe.substring(1, safe.length - 1);
-  if (SAFE_SERIALIZE_CHECK.test(unquoted)) {
-    return unquoted.replace(/<|\u2028|\u2029/g, serializeChar);
+  let result = '';
+  let lastPos = 0;
+  let replacement: string | undefined;
+  for (let i = 0, len = str.length; i < len; i++) {
+    replacement = serializeChar(str[i]);
+    if (replacement) {
+      result += str.slice(lastPos, i) + replacement;
+      lastPos = i + 1;
+    }
   }
-  return unquoted;
+  if (lastPos === 0) {
+    result = str;
+  } else {
+    result += str.slice(lastPos);
+  }
+  return result;
 }
 
 export function deserializeString(str: string): string {
-  const restored = str.replace(/\\x3C/g, '<')
+  return str
+    .replace(/\\"/g, '"')
+    .replace(/\\\\/g, '\\')
+    .replace(/\\n/g, '\n')
+    .replace(/\\r/g, '\r')
+    .replace(/\\b/g, '\b')
+    .replace(/\\t/g, '\t')
+    .replace(/\\f/g, '\f')
+    .replace(/\\x3C/g, '<')
     .replace(/\\u2028/g, '\u2028')
     .replace(/\\u2029/g, '\u2029');
-  return JSON.parse('"' + restored + '"') as string;
 }
