4887a3db2391342b561cbdf84a545dead1dd8a91
Alexis Munsayac <alexis.munsayac@gmail.com>
2023-04-09 15:54:01 +0800

    Add `Blob` tests

---
+++ packages/seroval/test/web-api/blob.test.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect } from 'vitest';
+import 'node-fetch-native/polyfill';
+import {
+  deserialize,
+  fromJSON,
+  serializeAsync,
+  toJSONAsync,
+} from '../../src';
+
+describe('Blob', () => {
+  describe('serializeAsync', () => {
+    it('supports Blob', async () => {
+      const example = new Blob(['Hello World'], {
+        type: 'text/plain',
+      });
+      const result = await serializeAsync(Promise.resolve(example));
+      expect(result).toMatchSnapshot();
+      const back = await deserialize<Promise<Blob>>(result);
+      expect(back).toBeInstanceOf(Blob);
+      expect(await back.text()).toBe(await example.text());
+      expect(back.type).toBe(example.type);
+    });
+  });
+  describe('toJSONAsync', () => {
+    it('supports Blob', async () => {
+      const example = new Blob(['Hello World'], {
+        type: 'text/plain',
+      });
+      const result = await toJSONAsync(Promise.resolve(example));
+      expect(JSON.stringify(result)).toMatchSnapshot();
+      const back = await fromJSON<Promise<Blob>>(result);
+      expect(back).toBeInstanceOf(Blob);
+      expect(await back.text()).toBe(await example.text());
+      expect(back.type).toBe(example.type);
+    });
+  });
+});
