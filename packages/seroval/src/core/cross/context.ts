7d87097b3751221a00407990b690488c06ddaa07
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-09-07 22:18:14 +0800

    Update context.ts

--- packages/seroval/src/core/cross/context.ts
+++ packages/seroval/src/core/cross/context.ts
@@ -1,4 +1,7 @@
+import type { Assignment, FlaggedObject } from '../assignments';
 import { ALL_ENABLED } from '../compat';
+import type { BaseParserContext } from '../context';
+import type { SerovalNode } from '../types';
 
 export interface CrossParserContextOptions {
   refs?: Map<unknown, number>;
@@ -28,4 +31,47 @@ export function createCrossIndexedValue<T>(
     return id;
   }
   return ref;
+}
+
+export interface StreamingCrossParserContextOptions extends CrossParserContextOptions {
+  onParse: (node: SerovalNode) => void;
+}
+
+export interface StreamingCrossParserContext extends CrossParserContext {
+  alive: boolean;
+  onParse(node: SerovalNode): void;
+}
+
+export function createStreamingCrossParserContext(
+  options: StreamingCrossParserContextOptions,
+): StreamingCrossParserContext {
+  return {
+    alive: true,
+    refs: options.refs || new Map<unknown, number>(),
+    features: ALL_ENABLED ^ (options.disabledFeatures || 0),
+    onParse: options.onParse,
+  };
+}
+
+export interface CrossSerializationContext extends BaseParserContext {
+  stack: number[];
+  // Array of assignments to be done (used for recursion)
+  assignments: Assignment[];
+  // Object flags
+  flags: FlaggedObject[];
+}
+
+export interface CrossSerializationOptions {
+  features: number;
+}
+
+export function createCrossSerializationContext(
+  options: CrossSerializationOptions,
+): CrossSerializationContext {
+  return {
+    stack: [],
+    assignments: [],
+    features: options.features,
+    flags: [],
+  };
 }
