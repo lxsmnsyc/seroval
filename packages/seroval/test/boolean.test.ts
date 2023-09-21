96452d3cabae3fd1afea0de73cdef76f46f56844
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-09-21 19:55:43 +0800

    Add boolean tests

--- packages/seroval/test/boolean.test.ts
+++ packages/seroval/test/boolean.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from 'vitest';
-import { serialize, serializeAsync } from '../src';
+import {
+  crossSerialize, crossSerializeAsync, crossSerializeStream, serialize, serializeAsync,
+} from '../src';
 
 describe('boolean', () => {
   describe('serialize', () => {
@@ -10,8 +12,42 @@ describe('boolean', () => {
   });
   describe('serializeAsync', () => {
     it('supports boolean', async () => {
-      expect(await serializeAsync(true)).toBe('!0');
-      expect(await serializeAsync(false)).toBe('!1');
+      expect(await serializeAsync(Promise.resolve(true))).toBe('Promise.resolve(!0)');
+      expect(await serializeAsync(Promise.resolve(false))).toBe('Promise.resolve(!1)');
     });
+  });
+  describe('crossSerialize', () => {
+    it('supports boolean', () => {
+      expect(crossSerialize(true)).toMatchSnapshot();
+      expect(crossSerialize(false)).toMatchSnapshot();
+    });
+  });
+  describe('crossSerializeAsync', () => {
+    it('supports boolean', async () => {
+      expect(await crossSerializeAsync(Promise.resolve(true))).toMatchSnapshot();
+      expect(await crossSerializeAsync(Promise.resolve(false))).toMatchSnapshot();
+    });
+  });
+  describe('crossSerializeAsync', () => {
+    it('supports true value', async () => new Promise<void>((done) => {
+      crossSerializeStream(Promise.resolve(true), {
+        onSerialize(data) {
+          expect(data).toMatchSnapshot();
+        },
+        onDone() {
+          done();
+        },
+      });
+    }));
+    it('supports false value', async () => new Promise<void>((done) => {
+      crossSerializeStream(Promise.resolve(false), {
+        onSerialize(data) {
+          expect(data).toMatchSnapshot();
+        },
+        onDone() {
+          done();
+        },
+      });
+    }));
   });
 });
