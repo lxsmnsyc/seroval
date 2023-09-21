316da69d4d8e86e150045fbb7a26107eb1869101
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-09-21 20:06:07 +0800

    Update boolean.test.ts

--- packages/seroval/test/boolean.test.ts
+++ packages/seroval/test/boolean.test.ts
@@ -1,6 +1,12 @@
 import { describe, it, expect } from 'vitest';
 import {
-  crossSerialize, crossSerializeAsync, crossSerializeStream, serialize, serializeAsync,
+  crossSerialize,
+  crossSerializeAsync,
+  crossSerializeStream,
+  serialize,
+  serializeAsync,
+  toJSON,
+  toJSONAsync,
 } from '../src';
 
 describe('boolean', () => {
@@ -14,6 +20,22 @@ describe('boolean', () => {
     it('supports boolean', async () => {
       expect(await serializeAsync(Promise.resolve(true))).toBe('Promise.resolve(!0)');
       expect(await serializeAsync(Promise.resolve(false))).toBe('Promise.resolve(!1)');
+    });
+  });
+  describe('toJSON', () => {
+    it('supports boolean', () => {
+      expect(JSON.stringify(toJSON(true))).toMatchSnapshot();
+      expect(JSON.stringify(toJSON(false))).toMatchSnapshot();
+    });
+  });
+  describe('serializeAsync', () => {
+    it('supports boolean', async () => {
+      expect(
+        JSON.stringify(await toJSONAsync(Promise.resolve(true))),
+      ).toMatchSnapshot();
+      expect(
+        JSON.stringify(await toJSONAsync(Promise.resolve(false))),
+      ).toMatchSnapshot();
     });
   });
   describe('crossSerialize', () => {
