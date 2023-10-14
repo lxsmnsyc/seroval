babd5ac0c864e5507dd629c4e889f7c6c71b2dc8
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-10-14 16:21:16 +0800

    Fix tests

--- packages/seroval/test.js
+++ packages/seroval/test.js
@@ -1,7 +1,7 @@
 
 import { serialize } from './dist/esm/development/index.mjs';
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10000; i++) {
   serialize({
     simple: true,
   })
---
+++ packages/seroval/test.ts
@@ -0,0 +1,26 @@
+import { createPlugin, SerovalNode } from '.';
+
+const BufferPlugin = createPlugin<Buffer, SerovalNode>({
+  tag: 'Buffer',
+  test(value) {
+    return value instanceof Buffer;
+  },
+  parse: {
+    sync(value, ctx) {
+      return ctx.parse(value.toString('base64'));
+    },
+    async async(value, ctx) {
+      ctx
+      return ctx.parse(value.toString('base64'));
+    },
+    stream(value, ctx) {
+      return ctx.parse(value.toString('base64'));
+    },
+  },
+  serialize(node, ctx) {
+    return `Buffer.from(${ctx.serialize(node)}, "base64")`;
+  },
+  deserialize(node, ctx) {
+    return Buffer.from(ctx.deserialize(node) as string, 'base64');
+  },
+});
