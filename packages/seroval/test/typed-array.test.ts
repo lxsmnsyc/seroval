eab64517f8a6b2723add3bc767d9606729ed0b5c
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-11-17 13:55:11 +0800

    Fix typed-array test

--- packages/seroval/test/typed-array.test.ts
+++ packages/seroval/test/typed-array.test.ts
@@ -9,138 +9,95 @@ import {
   crossSerialize,
   crossSerializeAsync,
   crossSerializeStream,
+  toCrossJSON,
+  fromCrossJSON,
+  toCrossJSONAsync,
+  toCrossJSONStream,
 } from '../src';
+
+const EXAMPLE = new Uint32Array([
+  0xFFFFFFFF,
+  0xFFFFFFFF,
+  0xFFFFFFFF,
+  0xFFFFFFFF,
+]);
 
 describe('typed arrays', () => {
   describe('serialize', () => {
     it('supports typed arrays', () => {
-      const example = new Uint32Array([
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-      ]);
-      const result = serialize(example);
+      const result = serialize(EXAMPLE);
       expect(result).toMatchSnapshot();
-      const back = deserialize<Uint32Array>(result);
+      const back = deserialize<typeof EXAMPLE>(result);
       expect(back).toBeInstanceOf(Uint32Array);
-      expect(back[0]).toBe(example[0]);
-      expect(back[1]).toBe(example[1]);
-      expect(back[2]).toBe(example[2]);
-      expect(back[3]).toBe(example[3]);
+      expect(back[0]).toBe(EXAMPLE[0]);
+      expect(back[1]).toBe(EXAMPLE[1]);
+      expect(back[2]).toBe(EXAMPLE[2]);
+      expect(back[3]).toBe(EXAMPLE[3]);
     });
   });
   describe('serializeAsync', () => {
     it('supports typed arrays', async () => {
-      const example = new Uint32Array([
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-      ]);
-      const result = await serializeAsync(Promise.resolve(example));
+      const result = await serializeAsync(Promise.resolve(EXAMPLE));
       expect(result).toMatchSnapshot();
-      const back = await deserialize<Promise<Uint32Array>>(result);
+      const back = await deserialize<Promise<typeof EXAMPLE>>(result);
       expect(back).toBeInstanceOf(Uint32Array);
-      expect(back[0]).toBe(example[0]);
-      expect(back[1]).toBe(example[1]);
-      expect(back[2]).toBe(example[2]);
-      expect(back[3]).toBe(example[3]);
+      expect(back[0]).toBe(EXAMPLE[0]);
+      expect(back[1]).toBe(EXAMPLE[1]);
+      expect(back[2]).toBe(EXAMPLE[2]);
+      expect(back[3]).toBe(EXAMPLE[3]);
     });
   });
   describe('toJSON', () => {
     it('supports typed arrays', () => {
-      const example = new Uint32Array([
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-      ]);
-      const result = toJSON(example);
+      const result = toJSON(EXAMPLE);
       expect(JSON.stringify(result)).toMatchSnapshot();
-      const back = fromJSON<Uint32Array>(result);
+      const back = fromJSON<typeof EXAMPLE>(result);
       expect(back).toBeInstanceOf(Uint32Array);
-      expect(back[0]).toBe(example[0]);
-      expect(back[1]).toBe(example[1]);
-      expect(back[2]).toBe(example[2]);
-      expect(back[3]).toBe(example[3]);
+      expect(back[0]).toBe(EXAMPLE[0]);
+      expect(back[1]).toBe(EXAMPLE[1]);
+      expect(back[2]).toBe(EXAMPLE[2]);
+      expect(back[3]).toBe(EXAMPLE[3]);
     });
   });
   describe('toJSONAsync', () => {
     it('supports typed arrays', async () => {
-      const example = new Uint32Array([
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-      ]);
-      const result = await toJSONAsync(Promise.resolve(example));
+      const result = await toJSONAsync(Promise.resolve(EXAMPLE));
       expect(JSON.stringify(result)).toMatchSnapshot();
-      const back = await fromJSON<Promise<Uint32Array>>(result);
+      const back = await fromJSON<Promise<typeof EXAMPLE>>(result);
       expect(back).toBeInstanceOf(Uint32Array);
-      expect(back[0]).toBe(example[0]);
-      expect(back[1]).toBe(example[1]);
-      expect(back[2]).toBe(example[2]);
-      expect(back[3]).toBe(example[3]);
+      expect(back[0]).toBe(EXAMPLE[0]);
+      expect(back[1]).toBe(EXAMPLE[1]);
+      expect(back[2]).toBe(EXAMPLE[2]);
+      expect(back[3]).toBe(EXAMPLE[3]);
     });
   });
   describe('crossSerialize', () => {
     it('supports typed arrays', () => {
-      const example = new Uint32Array([
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-      ]);
-      const result = crossSerialize(example);
+      const result = crossSerialize(EXAMPLE);
       expect(result).toMatchSnapshot();
     });
     describe('scoped', () => {
       it('supports typed arrays', () => {
-        const example = new Uint32Array([
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-        ]);
-        const result = crossSerialize(example, { scopeId: 'example' });
+        const result = crossSerialize(EXAMPLE, { scopeId: 'example' });
         expect(result).toMatchSnapshot();
       });
     });
   });
   describe('crossSerializeAsync', () => {
     it('supports typed arrays', async () => {
-      const example = new Uint32Array([
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-      ]);
-      const result = await crossSerializeAsync(Promise.resolve(example));
+      const result = await crossSerializeAsync(Promise.resolve(EXAMPLE));
       expect(result).toMatchSnapshot();
     });
     describe('scoped', () => {
       it('supports typed arrays', async () => {
-        const example = new Uint32Array([
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-        ]);
-        const result = await crossSerializeAsync(Promise.resolve(example), { scopeId: 'example' });
+        const result = await crossSerializeAsync(Promise.resolve(EXAMPLE), { scopeId: 'example' });
         expect(result).toMatchSnapshot();
       });
     });
   });
   describe('crossSerializeStream', () => {
     it('supports typed arrays', async () => new Promise<void>((done) => {
-      const example = new Uint32Array([
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-        0xFFFFFFFF,
-      ]);
-      crossSerializeStream(Promise.resolve(example), {
+      crossSerializeStream(Promise.resolve(EXAMPLE), {
         onSerialize(data) {
           expect(data).toMatchSnapshot();
         },
@@ -151,13 +108,7 @@ describe('typed arrays', () => {
     }));
     describe('scoped', () => {
       it('supports typed arrays', async () => new Promise<void>((done) => {
-        const example = new Uint32Array([
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-          0xFFFFFFFF,
-        ]);
-        crossSerializeStream(Promise.resolve(example), {
+        crossSerializeStream(Promise.resolve(EXAMPLE), {
           scopeId: 'example',
           onSerialize(data) {
             expect(data).toMatchSnapshot();
@@ -168,5 +119,45 @@ describe('typed arrays', () => {
         });
       }));
     });
+  });
+  describe('toCrossJSON', () => {
+    it('supports typed arrays', () => {
+      const result = toCrossJSON(EXAMPLE);
+      expect(JSON.stringify(result)).toMatchSnapshot();
+      const back = fromCrossJSON<typeof EXAMPLE>(result, {
+        refs: new Map(),
+      });
+      expect(back).toBeInstanceOf(Uint32Array);
+      expect(back[0]).toBe(EXAMPLE[0]);
+      expect(back[1]).toBe(EXAMPLE[1]);
+      expect(back[2]).toBe(EXAMPLE[2]);
+      expect(back[3]).toBe(EXAMPLE[3]);
+    });
+  });
+  describe('toCrossJSONAsync', () => {
+    it('supports typed arrays', async () => {
+      const result = await toCrossJSONAsync(Promise.resolve(EXAMPLE));
+      expect(JSON.stringify(result)).toMatchSnapshot();
+      const back = await fromCrossJSON<Promise<typeof EXAMPLE>>(result, {
+        refs: new Map(),
+      });
+      expect(back).toBeInstanceOf(Uint32Array);
+      expect(back[0]).toBe(EXAMPLE[0]);
+      expect(back[1]).toBe(EXAMPLE[1]);
+      expect(back[2]).toBe(EXAMPLE[2]);
+      expect(back[3]).toBe(EXAMPLE[3]);
+    });
+  });
+  describe('toCrossJSONStream', () => {
+    it('supports typed arrays', async () => new Promise<void>((done) => {
+      toCrossJSONStream(Promise.resolve(EXAMPLE), {
+        onParse(data) {
+          expect(JSON.stringify(data)).toMatchSnapshot();
+        },
+        onDone() {
+          done();
+        },
+      });
+    }));
   });
 });
