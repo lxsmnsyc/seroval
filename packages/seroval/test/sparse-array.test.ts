e56b5cd24b388390a38d3b99bdefc2d0959f7a14
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2023-11-17 13:50:46 +0800

    Fix sparse-array test

--- packages/seroval/test/sparse-array.test.ts
+++ packages/seroval/test/sparse-array.test.ts
@@ -9,85 +9,82 @@ import {
   crossSerialize,
   crossSerializeAsync,
   crossSerializeStream,
+  toCrossJSON,
+  fromCrossJSON,
+  toCrossJSONAsync,
+  toCrossJSONStream,
 } from '../src';
+
+const EXAMPLE = new Array(10);
 
 describe('sparse arrays', () => {
   describe('serialize', () => {
     it('supports sparse arrays', () => {
-      const example = new Array(10);
-      const result = serialize(example);
+      const result = serialize(EXAMPLE);
       expect(result).toMatchSnapshot();
-      const back = deserialize<undefined[]>(result);
+      const back = deserialize<typeof EXAMPLE>(result);
       expect(0 in back).toBeFalsy();
       expect(back[0]).toBe(undefined);
-      expect(back.length).toBe(example.length);
+      expect(back.length).toBe(EXAMPLE.length);
     });
   });
   describe('serializeAsync', () => {
     it('supports sparse arrays', async () => {
-      const example = new Array(10);
-      const result = await serializeAsync(Promise.resolve(example));
+      const result = await serializeAsync(Promise.resolve(EXAMPLE));
       expect(result).toMatchSnapshot();
-      const back = await deserialize<Promise<undefined[]>>(result);
+      const back = await deserialize<Promise<typeof EXAMPLE>>(result);
       expect(0 in back).toBeFalsy();
       expect(back[0]).toBe(undefined);
-      expect(back.length).toBe(example.length);
+      expect(back.length).toBe(EXAMPLE.length);
     });
   });
   describe('toJSON', () => {
     it('supports sparse arrays', () => {
-      const example = new Array(10);
-      const result = toJSON(example);
+      const result = toJSON(EXAMPLE);
       expect(JSON.stringify(result)).toMatchSnapshot();
-      const back = fromJSON<undefined[]>(result);
+      const back = fromJSON<typeof EXAMPLE>(result);
       expect(0 in back).toBeFalsy();
       expect(back[0]).toBe(undefined);
-      expect(back.length).toBe(example.length);
+      expect(back.length).toBe(EXAMPLE.length);
     });
   });
   describe('toJSONAsync', () => {
     it('supports sparse arrays', async () => {
-      const example = new Array(10);
-      const result = await toJSONAsync(Promise.resolve(example));
+      const result = await toJSONAsync(Promise.resolve(EXAMPLE));
       expect(JSON.stringify(result)).toMatchSnapshot();
-      const back = await fromJSON<Promise<undefined[]>>(result);
+      const back = await fromJSON<Promise<typeof EXAMPLE>>(result);
       expect(0 in back).toBeFalsy();
       expect(back[0]).toBe(undefined);
-      expect(back.length).toBe(example.length);
+      expect(back.length).toBe(EXAMPLE.length);
     });
   });
   describe('crossSerialize', () => {
     it('supports sparse arrays', () => {
-      const example = new Array(10);
-      const result = crossSerialize(example);
+      const result = crossSerialize(EXAMPLE);
       expect(result).toMatchSnapshot();
     });
     describe('scoped', () => {
       it('supports sparse arrays', () => {
-        const example = new Array(10);
-        const result = crossSerialize(example, { scopeId: 'example' });
+        const result = crossSerialize(EXAMPLE, { scopeId: 'example' });
         expect(result).toMatchSnapshot();
       });
     });
   });
   describe('crossSerializeAsync', () => {
     it('supports sparse arrays', async () => {
-      const example = new Array(10);
-      const result = await crossSerializeAsync(Promise.resolve(example));
+      const result = await crossSerializeAsync(Promise.resolve(EXAMPLE));
       expect(result).toMatchSnapshot();
     });
     describe('scoped', () => {
       it('supports sparse arrays', async () => {
-        const example = new Array(10);
-        const result = await crossSerializeAsync(Promise.resolve(example), { scopeId: 'example' });
+        const result = await crossSerializeAsync(Promise.resolve(EXAMPLE), { scopeId: 'example' });
         expect(result).toMatchSnapshot();
       });
     });
   });
   describe('crossSerializeStream', () => {
     it('supports sparse arrays', async () => new Promise<void>((done) => {
-      const example = new Array(10);
-      crossSerializeStream(Promise.resolve(example), {
+      crossSerializeStream(Promise.resolve(EXAMPLE), {
         onSerialize(data) {
           expect(data).toMatchSnapshot();
         },
@@ -98,8 +95,7 @@ describe('sparse arrays', () => {
     }));
     describe('scoped', () => {
       it('supports sparse arrays', async () => new Promise<void>((done) => {
-        const example = new Array(10);
-        crossSerializeStream(Promise.resolve(example), {
+        crossSerializeStream(Promise.resolve(EXAMPLE), {
           scopeId: 'example',
           onSerialize(data) {
             expect(data).toMatchSnapshot();
@@ -110,5 +106,41 @@ describe('sparse arrays', () => {
         });
       }));
     });
+  });
+  describe('toCrossJSON', () => {
+    it('supports sparse arrays', () => {
+      const result = toCrossJSON(EXAMPLE);
+      expect(JSON.stringify(result)).toMatchSnapshot();
+      const back = fromCrossJSON<typeof EXAMPLE>(result, {
+        refs: new Map(),
+      });
+      expect(0 in back).toBeFalsy();
+      expect(back[0]).toBe(undefined);
+      expect(back.length).toBe(EXAMPLE.length);
+    });
+  });
+  describe('toCrossJSONAsync', () => {
+    it('supports sparse arrays', async () => {
+      const result = await toCrossJSONAsync(Promise.resolve(EXAMPLE));
+      expect(JSON.stringify(result)).toMatchSnapshot();
+      const back = await fromCrossJSON<Promise<typeof EXAMPLE>>(result, {
+        refs: new Map(),
+      });
+      expect(0 in back).toBeFalsy();
+      expect(back[0]).toBe(undefined);
+      expect(back.length).toBe(EXAMPLE.length);
+    });
+  });
+  describe('toCrossJSONStream', () => {
+    it('supports sparse arrays', async () => new Promise<void>((done) => {
+      toCrossJSONStream(Promise.resolve(EXAMPLE), {
+        onParse(data) {
+          expect(JSON.stringify(data)).toMatchSnapshot();
+        },
+        onDone() {
+          done();
+        },
+      });
+    }));
   });
 });
