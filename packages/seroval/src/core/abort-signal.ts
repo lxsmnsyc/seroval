825dc7738be2a378f37a89a5bb502b1222630849
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-01-26 21:44:20 +0800

    Fix closures

--- packages/seroval/src/core/abort-signal.ts
+++ packages/seroval/src/core/abort-signal.ts
@@ -1,13 +1,19 @@
-export function abortSignalToPromise(signal: AbortSignal): Promise<any> {
-  return new Promise(resolve => {
-    signal.addEventListener(
-      'abort',
-      () => {
-        resolve(signal.reason);
-      },
-      {
-        once: true,
-      },
-    );
+function resolveAbortSignalResult(
+  this: AbortSignal,
+  resolve: (value: any) => void,
+): void {
+  resolve(this.reason);
+}
+
+function resolveAbortSignal(
+  this: AbortSignal,
+  resolve: (value: any) => void,
+): void {
+  this.addEventListener('abort', resolveAbortSignalResult.bind(this, resolve), {
+    once: true,
   });
+}
+
+export function abortSignalToPromise(signal: AbortSignal): Promise<any> {
+  return new Promise(resolveAbortSignal.bind(signal));
 }
