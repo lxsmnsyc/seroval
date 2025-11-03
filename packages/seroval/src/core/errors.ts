ce93def729933e7007ad71117e9e6671294eea43
Alexis H. Munsayac <alexis.munsayac@gmail.com>
2025-11-03 10:50:41 +0800

    Update errors.ts

--- packages/seroval/src/core/errors.ts
+++ packages/seroval/src/core/errors.ts
@@ -1,9 +1,17 @@
+/// <reference types="pridepack/env" />
+
 import { serializeString } from './string';
 import type { SerovalNode } from './types';
 
 const { toString: objectToString } = /* @__PURE__ */ Object.prototype;
 
-function getErrorMessage(type: string, cause: any): string {
+const enum StepErrorCodes {
+  Parse = 1,
+  Serialize = 2,
+  Deserialize = 3,
+}
+
+function getErrorMessageDev(type: string, cause: any): string {
   if (cause instanceof Error) {
     return `Seroval caught an error during the ${type} process.
   
@@ -19,6 +27,21 @@ ${cause.message}
 
 For more information, please check the "cause" property of this error.`;
 }
+
+const STEP_ERROR_CODES: Record<string, StepErrorCodes> = {
+  parsing: StepErrorCodes.Parse,
+  serialization: StepErrorCodes.Serialize,
+  deserialization: StepErrorCodes.Deserialize,
+};
+
+function getErrorMessageProd(type: string): string {
+  return `Seroval Error (step: ${STEP_ERROR_CODES[type]})`;
+}
+
+const getErrorMessage = (type: string, cause: any) =>
+  import.meta.env.PROD
+    ? getErrorMessageProd(type)
+    : getErrorMessageDev(type, cause);
 
 export class SerovalError extends Error {
   constructor(
@@ -47,10 +70,26 @@ export class SerovalDeserializationError extends SerovalError {
   }
 }
 
+const enum SpecificErrorCodes {
+  UnsupportedType = 1,
+  UnsupportedNode = 2,
+  MissingPlugin = 3,
+  MissingInstance = 4,
+  MissingReference = 5,
+  MissingReferenceForId = 6,
+  UnknownTypedArray = 7,
+}
+
+function getSpecificErrorMessage(code: SpecificErrorCodes): string {
+  return `Seroval Error (specific: ${code})`;
+}
+
 export class SerovalUnsupportedTypeError extends Error {
   constructor(public value: unknown) {
     super(
-      `The value ${objectToString.call(value)} of type "${typeof value}" cannot be parsed/serialized.
+      import.meta.env.PROD
+        ? getSpecificErrorMessage(SpecificErrorCodes.UnsupportedType)
+        : `The value ${objectToString.call(value)} of type "${typeof value}" cannot be parsed/serialized.
       
 There are few workarounds for this problem:
 - Transform the value in a way that it can be serialized.
@@ -61,42 +100,64 @@ There are few workarounds for this problem:
 
 export class SerovalUnsupportedNodeError extends Error {
   constructor(node: SerovalNode) {
-    super('Unsupported node type "' + node.t + '".');
+    super(
+      import.meta.env.PROD
+        ? getSpecificErrorMessage(SpecificErrorCodes.UnsupportedNode)
+        : 'Unsupported node type "' + node.t + '".',
+    );
   }
 }
 
 export class SerovalMissingPluginError extends Error {
   constructor(tag: string) {
-    super('Missing plugin for tag "' + tag + '".');
+    super(
+      import.meta.env.PROD
+        ? getSpecificErrorMessage(SpecificErrorCodes.MissingPlugin)
+        : 'Missing plugin for tag "' + tag + '".',
+    );
   }
 }
 
 export class SerovalMissingInstanceError extends Error {
   constructor(tag: string) {
-    super('Missing "' + tag + '" instance.');
+    super(
+      import.meta.env.PROD
+        ? getSpecificErrorMessage(SpecificErrorCodes.MissingInstance)
+        : 'Missing "' + tag + '" instance.',
+    );
   }
 }
 
 export class SerovalMissingReferenceError extends Error {
   constructor(public value: unknown) {
     super(
-      'Missing reference for the value "' +
-        objectToString.call(value) +
-        '" of type "' +
-        typeof value +
-        '"',
+      import.meta.env.PROD
+        ? getSpecificErrorMessage(SpecificErrorCodes.MissingReference)
+        : 'Missing reference for the value "' +
+            objectToString.call(value) +
+            '" of type "' +
+            typeof value +
+            '"',
     );
   }
 }
 
 export class SerovalMissingReferenceForIdError extends Error {
   constructor(id: string) {
-    super('Missing reference for id "' + serializeString(id) + '"');
+    super(
+      import.meta.env.PROD
+        ? getSpecificErrorMessage(SpecificErrorCodes.MissingReferenceForId)
+        : 'Missing reference for id "' + serializeString(id) + '"',
+    );
   }
 }
 
 export class SerovalUnknownTypedArrayError extends Error {
   constructor(name: string) {
-    super('Unknown TypedArray "' + name + '"');
+    super(
+      import.meta.env.PROD
+        ? getSpecificErrorMessage(SpecificErrorCodes.UnknownTypedArray)
+        : 'Unknown TypedArray "' + name + '"',
+    );
   }
 }
