5426ea8ce76802e0c69d9bc0f4f9242a8814c2c7
Sarah Gerrard <gerrardsarah@gmail.com>
2026-09-11 14:35:11 -0700

    perf: check extensibility first when reading object flags
    Extensible objects can be neither sealed nor frozen, so the common case
    needs one native call instead of three.

--- packages/seroval/src/core/utils/get-object-flag.ts
+++ packages/seroval/src/core/utils/get-object-flag.ts
@@ -1,14 +1,16 @@
 import { SerovalObjectFlags } from '../constants';
 
 export function getObjectFlag(obj: unknown): SerovalObjectFlags {
+  // Extensible objects can be neither sealed nor frozen, so the common case
+  // needs a single native call.
+  if (Object.isExtensible(obj)) {
+    return SerovalObjectFlags.None;
+  }
   if (Object.isFrozen(obj)) {
     return SerovalObjectFlags.Frozen;
   }
   if (Object.isSealed(obj)) {
     return SerovalObjectFlags.Sealed;
-  }
-  if (Object.isExtensible(obj)) {
-    return SerovalObjectFlags.None;
   }
   return SerovalObjectFlags.NonExtensible;
 }
